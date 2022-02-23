5246a2738a33aa5bfcc989e8230724514572fb70
kuksag <georgiy.kuksa@gmail.com>
2022-02-23 19:51:51 +0300

    Add hard task

--- 220203/main.py
+++ 220203/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-from matrix import Matrix
+from matrix import Matrix, cached_mult
 from number import ArrayLike
 
 ARTIFACT_FOLDER = 'artifact'
@@ -20,7 +20,7 @@ def check_matrix_mult():
     b = Matrix(np.random.randint(0, 10, (10, 10)))
     if not os.path.exists(ARTIFACT_FOLDER):
         os.makedirs(ARTIFACT_FOLDER)
-    with open(os.path.join(ARTIFACT_FOLDER, 'matrix+.txt'), 'w+') as file:
+    with open(os.path.join(ARTIFACT_FOLDER, 'matrix@.txt'), 'w+') as file:
         file.write(str(a @ b))
 
 
@@ -29,7 +29,7 @@ def check_matrix_elem_mult():
     b = Matrix(np.random.randint(0, 10, (10, 10)))
     if not os.path.exists(ARTIFACT_FOLDER):
         os.makedirs(ARTIFACT_FOLDER)
-    with open(os.path.join(ARTIFACT_FOLDER, 'matrix@.txt'), 'w+') as file:
+    with open(os.path.join(ARTIFACT_FOLDER, 'matrix*.txt'), 'w+') as file:
         file.write(str(a * b))
 
 
@@ -47,6 +47,43 @@ def check_numbers():
         print(str(a / b), file=fd)
 
 
+def create_collision():
+    a = Matrix([[1, 2],
+                [3, 4]])
+    b = d = Matrix([[5, 6],
+                    [7, 8]])
+    c = Matrix([[9, 10],
+                [11, 12]])
+
+    assert hash(a) == hash(c)
+    assert a != c
+    assert (b == d)
+    assert (a @ b != c @ d)
+
+    if not os.path.exists(ARTIFACT_FOLDER):
+        os.makedirs(ARTIFACT_FOLDER)
+
+    with open(os.path.join(ARTIFACT_FOLDER, 'A.txt'), 'w+') as fd:
+        print(a, file=fd)
+
+    with open(os.path.join(ARTIFACT_FOLDER, 'B.txt'), 'w+') as fd:
+        print(b, file=fd)
+
+    with open(os.path.join(ARTIFACT_FOLDER, 'C.txt'), 'w+') as fd:
+        print(c, file=fd)
+
+    with open(os.path.join(ARTIFACT_FOLDER, 'D.txt'), 'w+') as fd:
+        print(d, file=fd)
+
+    with open(os.path.join(ARTIFACT_FOLDER, 'AB.txt'), 'w+') as fd:
+        print(a @ b, file=fd)
+
+    with open(os.path.join(ARTIFACT_FOLDER, 'CD.txt'), 'w+') as fd:
+        print(c @ d, file=fd)
+
+    with open(os.path.join(ARTIFACT_FOLDER, 'hash.txt'), 'w+') as fd:
+        print(hash(a @ b), file=fd)
+        print(hash(c @ d), file=fd)
 
 
 if __name__ == '__main__':
@@ -55,3 +92,4 @@ if __name__ == '__main__':
     check_matrix_mult()
     check_matrix_elem_mult()
     check_numbers()
+    create_collision()
--- 220203/matrix.py
+++ 220203/matrix.py
@@ -1,18 +1,37 @@
-from attrs import define, field
+import numpy as np
 from copy import copy
+from functools import lru_cache
 
 
-def _check_dimensions(instance, attribute, value):
-    flag = True
-    for row in value:
-        flag &= len(row) == len(value)
-    if not flag:
-        raise ValueError("Bad dimensions")
+class PerfectHash:
+    """
+    hash(matrix) = 3 * |rows| + 5 * |cols| + 13
+    """
+
+    def __eq__(self, other):
+        return np.array_equal(self.data, other.data)
+
+    def __hash__(self):
+        return 3 * self.data.shape[0] + 5 * self.data.shape[1] + 13
 
 
-@define(frozen=True)
-class Matrix:
-    data = field(validator=_check_dimensions)
+class Matrix(PerfectHash):
+    @staticmethod
+    def _check_dimensions(value):
+        flag = True
+        for row in value:
+            flag &= len(row) == len(value)
+        if not flag:
+            raise ValueError("Bad dimensions")
+
+    def __init__(self, data):
+        self._check_dimensions(data)
+        self.data = np.asarray(data)
+
+    __hash__ = PerfectHash.__hash__
+
+    def __repr__(self):
+        return 'Matrix' + str(self.data)
 
     def __add__(self, other):
         if self.data.shape != other.data.shape:
@@ -35,3 +54,12 @@ class Matrix:
         for i, row in enumerate(other.data):
             result[i] *= row
         return Matrix(result)
+
+
+cache = dict()
+
+
+def cached_mult(lhs: Matrix, rhs: Matrix) -> Matrix:
+    if hash((lhs, rhs)) not in cache:
+        cache[hash((lhs, rhs))] = lhs @ rhs
+    return cache[hash((lhs, rhs))]
